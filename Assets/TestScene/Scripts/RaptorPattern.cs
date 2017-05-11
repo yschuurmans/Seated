a67ac01635530289059e06ad86f5ab1c42a16656
Dirk van der Meulen <i279141>
2017-05-11 15:46:51 +0200

    Added raptorInput class. Added input changed events. Added Input list

---
+++ Assets/TestScene/Scripts/RaptorPattern.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.TestScene.Scripts
+{
+    public abstract class RaptorPattern
+    {
+        
+        protected ushort[,] RaptorHead;
+        protected ushort[,] RaptorBack;
+        protected ushort[,] RaptorSeat;
+
+        public IEnumerator RunPattern(/*float durationInSeconds = DefaultDurationInSeconds, float delayInSeconds = 0*/)
+        {
+            yield break;
+        }
+    }
+}
