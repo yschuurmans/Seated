ce4f715a5821a1789807126378e8d04a04f7b638
Youri Schuurmans <yourish@live.nl>
2017-05-18 10:41:57 +0200

    working on lift

---
+++ Assets/Scripts/LiftMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiftMovement : MonoBehaviour
+{
+    private Transform thisTransform;
+    public float Lift;
+    public Vector3 Direction;
+    private void Awake()
+    {
+        thisTransform = transform;
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
