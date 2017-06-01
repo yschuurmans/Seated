d3fc554a190bf6e1bbfd98bac07d192b9817dcd5
Thomas <thomasvandongen019@hotmail.com>
2017-06-01 15:48:37 +0200

    Start met challenges.

---
+++ Assets/Classes/DeliverChallenge.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class DeliverChallenge : Challenge{
+    private string _item;
+
+    public DeliverChallenge(string name, string description, Vector3 startLocation, Vector3 location, string item) : base(name, description, startLocation, location)
+    {
+        _item = item;
+    }
+
+    public string GetItem()
+    {
+        return _item;
+    }
+}
---
+++ Assets/Classes/TimeChallenge.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class TimeChallenge : Challenge {
+    /// <summary>
+    /// time of the challenge in seconds
+    /// </summary>
+    private int _time;
+
+    public TimeChallenge(string name, string description, Vector3 startLocation, Vector3 location, int time) : base(name, description, startLocation, location)
+    {
+        _time = time;
+    }
+
+    public int GetTime()
+    {
+        return _time;
+    }
+
+}
