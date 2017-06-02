a84e84e6ae9ea26e948eb0dd0444de8f4c540a35
Youri Schuurmans <yourish@live.nl>
2017-06-02 14:02:07 +0200

    gotta push

--- Assets/Scripts/LiftMovement.cs
+++ Assets/Scripts/LiftMovement.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -6,82 +7,93 @@ using UnityEngine.UI;
 
 public class LiftMovement : MonoBehaviour
 {
-    public Text velText;
-    public float LiftMultiplier = 0.75f;
-    public float UpwardLiftRatio = 1f;
-    public float ForwardLiftRatio = 3f;
-    public float GravModifierImpact = 1f;
-    public float PitchResistance;
-    public float RollResistance;
 
+    public Text velText;
+    public float LiftMultiplier = 1;
+    public float AreaMultiplier = 1;
+    public float AirDensity;
+    public float Coefficient;
 
     private Transform tt;
     private Rigidbody rb;
-    public float Lift;
-    private List<float> RecentLift;
-    private float LiftGravModifier;
-    private float VelGravModifier;
-    private Vector3 Direction;
     private Vector3 LocalVelocity;
+    private float VelocityLastFrame = 0f;
+    private float Velocity = 0f;
+
+    public float Lift;
+    public float Area;
+
+    public WingPointTransforms Wings;
+    [Serializable]
+    public class WingPointTransforms
+    {
+        public Transform Left;
+        public Transform Right;
+        public Transform Front;
+        public Transform Back;
+    }
+
+
     private void Awake()
     {
         tt = transform;
         rb = GetComponent<Rigidbody>();
-
-        RecentLift = new List<float>();
     }
     // Use this for initialization
     void Start()
     {
     }
 
-    private float CalculateLift(float velocity)
+    private float CalculateLift(float velocity, float area)
     {
         // https://www.grc.nasa.gov/www/K-12/airplane/lifteq.html
         //
         //     Cl * ( p * V^2 )                / 2 * A
-        //return 1 * (1 * Mathf.Pow(velocity, 2)) / 2 * 1;
 
-        return Mathf.Clamp(Mathf.Pow(velocity, 4f / 6f) * 3, velocity / 2, 8 * velocity);
+        return (Coefficient * ((AirDensity * Mathf.Pow(velocity, 2)) / 2) * area) * LiftMultiplier;
+    }
+
+    private float CalculateWingArea()
+    {
+        float width = Vector3.Distance(
+            new Vector3(Wings.Left.position.x, 0, Wings.Left.position.z),
+            new Vector3(Wings.Right.position.x, 0, Wings.Right.position.z));
+
+        float length = Vector3.Distance(
+            new Vector3(Wings.Front.position.x, 0, Wings.Front.position.z),
+            new Vector3(Wings.Back.position.x, 0, Wings.Back.position.z));
+
+        //float width = WingPoints.RightWingTip.position.x - WingPoints.LeftWingTip.position.x;
+        //float length = WingPoints.FrontWingTip.position.z - WingPoints.BackCenter.position.z;
+
+        return (width * length / 2) * AreaMultiplier;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        //LiftGravModifier = 1- Mathf.Abs(1 - (Vector3.Angle(Vector3.down, tt.forward) / 90));
 
-        //RecentLift.Add(CalculateLift(rb.velocity.magnitude * LiftGravModifier));
-        ////RecentLift.Add((CalculateLift(rb.velocity.magnitude * LiftGravModifier) + Lift) / 2);
-        //while (RecentLift.Count > 60) RecentLift.RemoveAt(0);
-        //Lift = RecentLift.Average();
+        Area = CalculateWingArea();
+        Velocity = rb.velocity.magnitude;
+        Lift = CalculateLift(Velocity, Area);
 
 
+        LocalVelocity = tt.up * Lift;
+
+        //LocalVelocity = tt.rotation * LocalVelocity;
+        rb.AddForce(LocalVelocity*Time.deltaTime);
+        rb.AddForce(tt.forward * (Velocity - VelocityLastFrame));
 
 
-        LiftGravModifier = Mathf.Clamp01(0.2f + Mathf.Clamp(Vector3.Angle(Vector3.up, tt.forward), 0, 70) / 70);
+        VelocityLastFrame = Velocity;
 
-        Lift = CalculateLift(rb.velocity.magnitude * LiftGravModifier);
+        //LocalVelocity = Vector3.Lerp(
+        //    rb.velocity - rb.velocity * 0.002f * Time.fixedDeltaTime,
+        //    LocalVelocity,
+        //    5 * Time.fixedDeltaTime)
+        //    + Physics.gravity * rb.mass;
 
-
-        LocalVelocity = Vector3.zero;
-
-        LocalVelocity.y = UpwardLiftRatio;
-        LocalVelocity.z = ForwardLiftRatio;
-        LocalVelocity = LocalVelocity.normalized * Lift * LiftMultiplier;
-
-        float downwardAngle = Vector3.Angle(Vector3.down, tt.forward);
-
-        VelGravModifier = (Mathf.Clamp(downwardAngle, 0, 90) / 90);
-
-        LocalVelocity = tt.rotation * LocalVelocity;
-
-        LocalVelocity = Vector3.Lerp(
-            rb.velocity - rb.velocity * 0.002f /** Time.fixedDeltaTime*/,
-            LocalVelocity,
-            5 * Time.fixedDeltaTime) 
-            + Physics.gravity * rb.mass * (VelGravModifier * GravModifierImpact);
-
-        rb.velocity = LocalVelocity;
+        //rb.velocity = LocalVelocity;
 
 
 
@@ -89,66 +101,47 @@ public class LiftMovement : MonoBehaviour
 
         //tt.position = new Vector3(0, 50, 0);
 
-        Debug.Log("angle:" + Mathf.Abs(tt.eulerAngles.x < 180 ? tt.eulerAngles.x : tt.eulerAngles.x - 360f));
+        //Debug.Log("angle:" + Mathf.Abs(tt.eulerAngles.x < 180 ? tt.eulerAngles.x : tt.eulerAngles.x - 360f));
 
-        float angleX = Mathf.Abs(tt.eulerAngles.x < 180 ? tt.eulerAngles.x : tt.eulerAngles.x - 360f);
-        float angleZ = Mathf.Abs(tt.eulerAngles.z < 180 ? tt.eulerAngles.z : tt.eulerAngles.z - 360f);
-        if (angleX < PitchResistance && angleZ < RollResistance)
-        {
-            tt.rotation = Quaternion.Lerp(tt.rotation, Quaternion.Euler(0, tt.eulerAngles.y, tt.eulerAngles.z), (1 - angleX / PitchResistance) * Time.deltaTime);
-            tt.rotation = Quaternion.Lerp(tt.rotation, Quaternion.Euler(tt.eulerAngles.x, tt.eulerAngles.y, 0), (1 - angleZ / RollResistance) * Time.deltaTime);
-        }
-        else if (angleX > 180 - PitchResistance && angleZ > 180 - RollResistance)
-        {
-            tt.rotation = Quaternion.Lerp(tt.rotation, Quaternion.Euler(180, tt.eulerAngles.y, tt.eulerAngles.z), (1 - angleX / PitchResistance) * Time.deltaTime);
-            tt.rotation = Quaternion.Lerp(tt.rotation, Quaternion.Euler(tt.eulerAngles.x, tt.eulerAngles.y, 180), (1 - angleZ / RollResistance) * Time.deltaTime);
-        }
+        //float angleX = Mathf.Abs(tt.eulerAngles.x < 180 ? tt.eulerAngles.x : tt.eulerAngles.x - 360f);
+        //float angleZ = Mathf.Abs(tt.eulerAngles.z < 180 ? tt.eulerAngles.z : tt.eulerAngles.z - 360f);
+        //if (angleX < PitchResistance && angleZ < RollResistance)
+        //{
+        //    tt.rotation = Quaternion.Lerp(tt.rotation, Quaternion.Euler(0, tt.eulerAngles.y, tt.eulerAngles.z), (1 - angleX / PitchResistance) * Time.deltaTime);
+        //    tt.rotation = Quaternion.Lerp(tt.rotation, Quaternion.Euler(tt.eulerAngles.x, tt.eulerAngles.y, 0), (1 - angleZ / RollResistance) * Time.deltaTime);
+        //}
+        //else if (angleX > 180 - PitchResistance && angleZ > 180 - RollResistance)
+        //{
+        //    tt.rotation = Quaternion.Lerp(tt.rotation, Quaternion.Euler(180, tt.eulerAngles.y, tt.eulerAngles.z), (1 - angleX / PitchResistance) * Time.deltaTime);
+        //    tt.rotation = Quaternion.Lerp(tt.rotation, Quaternion.Euler(tt.eulerAngles.x, tt.eulerAngles.y, 180), (1 - angleZ / RollResistance) * Time.deltaTime);
+        //}
 
 
 
-        velText.text = "Lift: " + Lift.ToString("F1") + "\nSpeed: " + rb.velocity.magnitude.ToString("F1") + "\nHeight: " + tt.position.y.ToString("F1") + "\nAngle: " + angleX.ToString("F1");
+        velText.text = "Area: "+ Area + "\nLift: " + Lift.ToString("F1") + "\nSpeed: " + rb.velocity.magnitude.ToString("F1") + "\nHeight: " + tt.position.y.ToString("F1");
     }
 
-    private List<Vector3> recentVelocitys = new List<Vector3>();
-    private List<Vector3> recentLocalVel = new List<Vector3>();
+
     void OnDrawGizmos()
     {
-        recentVelocitys.Add(rb.velocity);
-        while (recentVelocitys.Count > 10) recentVelocitys.RemoveAt(0);
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(rb.position, rb.position + rb.velocity);
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(rb.position, rb.position + LocalVelocity);
 
         Gizmos.color = Color.blue;
-        Vector3 avg = Vector3.zero;
-        recentVelocitys.ForEach(x => avg += x);
-        avg = avg / recentVelocitys.Count;
-        Gizmos.DrawLine(rb.position, rb.position + avg * 5);
-
-
-        LocalVelocity.y = Lift * UpwardLiftRatio;
-        LocalVelocity.z = Lift * ForwardLiftRatio;
-        LocalVelocity = Vector3.Lerp(rb.velocity, LocalVelocity, 0.2f);
-        LocalVelocity = tt.localRotation * LocalVelocity;
-
-        recentLocalVel.Add(LocalVelocity);
-        while (recentLocalVel.Count > 10) recentLocalVel.RemoveAt(0);
-
-        Gizmos.color = Color.red;
-        Vector3 avg2 = Vector3.zero;
-        recentLocalVel.ForEach(x => avg2 += x);
-        avg2 = (avg2 / recentLocalVel.Count);
-        Gizmos.DrawLine(rb.position, rb.position + avg2 * 5);
+        Gizmos.DrawLine(
+            new Vector3(Wings.Left.position.x, tt.position.y - 2, Wings.Left.position.z),
+            new Vector3(Wings.Back.position.x, tt.position.y - 2, Wings.Back.position.z));
+        Gizmos.DrawLine(
+            new Vector3(Wings.Back.position.x, tt.position.y - 2, Wings.Back.position.z),
+            new Vector3(Wings.Right.position.x, tt.position.y - 2, Wings.Right.position.z));
+        Gizmos.DrawLine(
+            new Vector3(Wings.Right.position.x, tt.position.y - 2, Wings.Right.position.z),
+            new Vector3(Wings.Front.position.x, tt.position.y - 2, Wings.Front.position.z));
+        Gizmos.DrawLine(
+            new Vector3(Wings.Front.position.x, tt.position.y - 2, Wings.Front.position.z),
+            new Vector3(Wings.Left.position.x, tt.position.y - 2, Wings.Left.position.z));
     }
-
-
-
-    //void OnDrawGizmos()
-    //{
-    //    Gizmos.color = Color.blue;
-    //    Gizmos.DrawLine(tt.position, tt.position + tt.forward * 5);
-
-    //    Gizmos.color = Color.red;
-    //    Gizmos.DrawLine(tt.position, tt.position + tt.right * 5);
-
-    //    Gizmos.color = Color.green;
-    //    Gizmos.DrawLine(tt.position, tt.position + tt.up * 5);
-    //}
 }
