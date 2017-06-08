efc6f0ef9d04bb3949c5aa8909138ee99a1d2e1b
Dirk van der Meulen <i279141>
2017-06-08 15:20:05 +0200

    Big update to challenges

--- Assets/TestScene/Scripts/DeltaFlyer.cs
+++ Assets/TestScene/Scripts/DeltaFlyer.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 public class DeltaFlyer : MonoBehaviour
 {
     public Raptor raptor;
+    
     public ContactPoint[] contactPoints { get { return raptor.contactPoints; } }
     public AirStream currentAirStream;
     public AirStream detectedAirStream;
