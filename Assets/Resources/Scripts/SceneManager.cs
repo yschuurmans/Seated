a6e1916ae25fdcdcb10d3ba0577311e1cf40628d
Aecnoril <Aecnorilkoen@gmail.com>
2017-06-23 12:01:18 +0200

    changing of level and exiting game

--- Assets/Resources/Scripts/SceneManager.cs
+++ Assets/Resources/Scripts/SceneManager.cs
@@ -1,22 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SceneManager : MonoBehaviour {
+
+    string levelName = "Island";
+    Text levelBtnText;
 
     void Awake ()
     {
         DontDestroyOnLoad(this);
+        foreach (Button b in FindObjectOfType<Canvas>().GetComponentsInChildren<Button>() )
+        { if (b.name == "ChangeLevelBtn")
+                levelBtnText = b.GetComponentInChildren<Text>(); }
     }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+	void Update()
+    {
+        if (levelName == "Island")
+            levelBtnText.text = "Level: Island";
+        else if (levelName == "PlayableScenarioScene")
+            levelBtnText.text = "Level: Canyon";
+
+        if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().name != "MainMenu" && Input.GetKey(KeyCode.Escape))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+        }
+    }
 
     public void StartGame()
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene("PlayableScenarioScene");
+        UnityEngine.SceneManagement.SceneManager.LoadScene(levelName);
+    }
+
+    public void ChangeLevel()
+    {
+        if (levelName == "Island") levelName = "PlayableScenarioScene";
+        else levelName = "Island";
     }
 
     public void ExitGame()
